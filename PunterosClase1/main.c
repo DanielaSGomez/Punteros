435d7e51f47d5d8952cbe4b775a21e7f93a0f5d8
alumno <alumno>
2019-05-14 12:28:58 -0300

    la

--- PunterosClase1/main.c
+++ PunterosClase1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//PRIMER CLASE PUNTEROS----------------------------------------------------------------------------------------------
 int main()
 {
     int* x = NULL;
@@ -23,14 +24,16 @@ int main()
 
     printf("\n%d",*x);
 
-   //////////pedir una variable char por puntero...//////////////////
+    //---------------------------------------------------------------------------------------------------------------
+   //pedir una variable char por puntero...
 
 
     //variable algo
     char ingresado;
     //regla de estilo  puntero a la variable algo : pAlgo
+    //hay que inicializar el puntero en null
     char* pIngresado = NULL;
-
+    //despues se le asigna la direccion de memoria a la que apunta, sino apunta a cualquier lado y rompe el programa
     pIngresado = &ingresado;
 
     printf("\nIngrese un caracter: ");
@@ -38,7 +41,23 @@ int main()
     scanf("%c",pIngresado);
     printf("%c",ingresado);
 
+    //--------------------------------------------------------------------------------------------------------------
 
+    int p;
+    int* q;
+    int* r;
+
+    p = 10;
+    q = &p;
+
+    printf("\n%p",q);
+    r = q;
+
+    printf("\n%p",r);
+
+    printf("\n%d",*q);
+
+    printf("\n%d",*r);
 
 
 
